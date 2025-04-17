602c5f3bab5b4499530463ecd276771c24778ecb
Bruno Silva-Cubillos <brunosilvacubillos@outlook.fr>
2025-04-17 10:12:15 -0400

    Save progress before updating Angular

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    // your components here
+  ],
+  imports: [
+    // other imports here
+    HttpClientModule,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
